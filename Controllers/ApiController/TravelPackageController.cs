bd8458453a2ea1fb7706f14a9c17b1b39cc2fdbc
Cix8 <francesco.natal_fn@libero.it>
2022-10-20 15:19:59 +0200

    add Home_view, display contents with axios from ApiController/TravelPackageController

---
+++ Controllers/ApiController/TravelPackageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using webapp_travel_agency.Db_Context;
+using webapp_travel_agency.Models;
+
+namespace webapp_travel_agency.Controllers.ApiController
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TravelPackageController : ControllerBase
+    {
+        private TravelAgencyContext _ctx;
+        public TravelPackageController()
+        {
+            _ctx = new TravelAgencyContext();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<TravelPackage> packages = _ctx.TravelPackages.ToList();
+            return Ok(packages);
+        }
+    }
+}
